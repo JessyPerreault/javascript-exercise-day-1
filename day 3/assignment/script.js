a513aa521fe1f89bdf296b5e7a2b4b7da54bc267
Jessyfromwebdev <jessy.perreault@edu.sait.ca>
2019-02-06 15:46:11 -0700

    day 3

---
+++ day 3/assignment/script.js
@@ -0,0 +1,42 @@
+var main = document.getElementsByTagName("main")[0];
+var newImageCat = document.createElement("img");
+var newImageDog = document.createElement("img");
+newImageCat.src = "http://placekitten.com/200/300";
+newImageDog.src = "http://www.placepuppy.net/400/250";
+newImageCat.style.border = "2px solid black"
+newImageDog.style.border = "2px solid black"
+newImageCat.style.borderRadius = "30px"
+newImageDog.style.borderRadius = "30px"
+
+if (Modernizr.geolocation) {
+    main.appendChild(newImageCat);
+  } else {
+    main.appendChild(newImageDog);
+  }
+
+
+// ???????????????????????????????????????????????? 
+// function(allDivs) {
+// var allDivs = document.querySelectorAll("div");
+
+// for (var i = 0; i < allDivs.length; i++) {
+//     var allDiv = allDivs[i];
+// } 
+// if (allDiv % 2 === 0) {
+//     allDivs.style.backgroundColor = "blue";
+// } else {
+//     allDivs.style.backgroundColor = "red";
+// }
+// } ??????????????????????????????????????????????
+
+
+
+
+
+
+
+
+
+
+// http://placekitten.com/200/300
+// http://www.placepuppy.net/400/250
